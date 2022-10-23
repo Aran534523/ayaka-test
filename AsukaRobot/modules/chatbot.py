2c4e73263e03354b8ab10e65808795fcde501c71
ARAN アラン <115202562+TaktAsahina@users.noreply.github.com>
2022-10-23 16:32:02 +0530

    Update chatbot.py

--- AsukaRobot/modules/chatbot.py
+++ AsukaRobot/modules/chatbot.py
@@ -117,7 +117,7 @@ def chatbot(update: Update, context: CallbackContext):
             return
         anon = message.text
         bot.send_chat_action(chat_id, action="typing")
-        url = f"https://kukiapi.xyz/api/apikey=1356469075-KUKIkq4WMg5FV4/Fallen/Anonymous/message={anon}" 
+        url = f"https://api.bakufu.tech/api/chatbot/cleverbot?name=DragonEye&owner=DRAGONEYE&message={anon}" 
         request = requests.get(url) 
         results = json.loads(request.text) 
         result = f"{results['reply']}"
