a849e2a6ceb93339e7fe710db7aaeabf214fcd47
ARAN アラン <115202562+TaktAsahina@users.noreply.github.com>
2022-11-01 17:53:02 +0530

    Create kazuha.py

---
+++ AsukaRobot/modules/kazuha.py
@@ -0,0 +1,47 @@
+from AsukaRobot import pgram
+
+@pgram.on_message(filters.command("void") & ~filters.edited & ~filters.bot)
+
+async def hmm(client, message):
+
+    PHOTO="https://telegra.ph/file/e5808adf6d1bc748d6440.jpg"
+
+    Text= f"""
+
+Welcome to [【V๏ɪ፝֟𝔡】Network](https://t.me/voidxnetwork)
+
+━━━━━━━━━━━━━━━━━━━━━━
+
+✪ ᴠᴏɪᴅ ɪꜱ ᴀɴ ᴀɴɪᴍᴇ ʙᴀꜱᴇᴅ ᴄᴏᴍᴍᴜɴɪᴛʏ ᴡɪᴛʜ ᴀ ᴍᴏᴛɪᴠᴇ ᴛᴏ ꜱᴘʀᴇᴀᴅ ʟᴏᴠᴇ ᴀɴᴅ ᴘᴇᴀᴄᴇ ᴀʀᴏᴜɴᴅ ᴛᴇʟᴇɢʀᴀᴍ.
+
+✪ ɢᴏ ᴛʜʀᴏᴜɢʜ ᴛʜᴇ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴊᴏɪɴ ᴛʜᴇ ᴄᴏᴍᴍᴜɴɪᴛʏ ɪꜰ ɪᴛ ᴅʀᴀᴡꜱ ʏᴏᴜʀ ᴀᴛᴛᴇɴᴛɪᴏɴ. 
+
+━━━━━━━━━━━━━━━━━━━━━━
+
+"""
+
+    await BOT.send_file(event.chat_id,
+
+                PHOTO,
+
+                caption=Text,
+
+                buttons=(
+
+                [
+
+                    [Button.url("【V๏ɪ፝֟𝔡】Network", "https://t.me/VoidXNetwork")],
+
+                    [
+
+                    Button.url("【ᴜꜱᴇʀᴛᴀɢ】", "https://t.me/VoidxNetwork/136"),
+
+                    Button.url("【ɪɴᴅᴇx】", "https://t.me/VoidxNetwork/15")
+
+                    ],
+
+                ]
+
+            ),
+
+        )
