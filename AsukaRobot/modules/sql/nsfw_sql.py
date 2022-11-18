dc43f1d5ea2574b68177a0cfa017e061e63ce7c4
ARAN アラン <115202562+Aran-Sama@users.noreply.github.com>
2022-11-18 17:11:39 +0530

    Update nsfw_sql.py

--- AsukaRobot/modules/sql/nsfw_sql.py
+++ AsukaRobot/modules/sql/nsfw_sql.py
@@ -1,26 +1,48 @@
 import threading
 from sqlalchemy import Column, String
-from AsukaRobot.modules.sql import db
+from AsukaRobot.modules.sql import BASE, SESSION
+#   |----------------------------------|
+#   |  Test Module by @Aran-Sama |
+#   |----------------------------------|
+class NSFWChats(BASE):
+    __tablename__ = "nsfw_chats"
+    chat_id = Column(String(14), primary_key=True)
 
-nsfwchatsdb = db.nsfwchats
+    def __init__(self, chat_id):
+        self.chat_id = chat_id
+
+NSFWChats.__table__.create(checkfirst=True)
+INSERTION_LOCK = threading.RLock()
+
 
 def is_nsfw(chat_id):
-    nsfwchat = nsfwchatsdb.find_one({"chat_id": chat_id})
-    if not nsfwchat:
-        return False
-    else:
-        return True
+    try:
+        chat = SESSION.query(NSFWChats).get(str(chat_id))
+        if chat:
+            return True
+        else:
+            return False
+    finally:
+        SESSION.close()
 
 def set_nsfw(chat_id):
-    nsfwchat = is_nsfw(chat_id)
-    if nsfwchat:
-        return
-    else:
-        return nsfwchatsdb.insert_one({"chat_id": chat_id})
+    with INSERTION_LOCK:
+        nsfwchat = SESSION.query(NSFWChats).get(str(chat_id))
+        if not nsfwchat:
+            nsfwchat = NSFWChats(str(chat_id))
+        SESSION.add(nsfwchat)
+        SESSION.commit()
 
 def rem_nsfw(chat_id):
-    nsfwchat = is_nsfw(chat_id)
-    if not nsfwchat:
-        return
-    else:
-        return nsfwchatsdb.delete_one({"chat_id": chat_id})
+    with INSERTION_LOCK:
+        nsfwchat = SESSION.query(NSFWChats).get(str(chat_id))
+        if nsfwchat:
+            SESSION.delete(nsfwchat)
+        SESSION.commit()
+
+
+def get_all_nsfw_chats():
+    try:
+        return SESSION.query(NSFWChats.chat_id).all()
+    finally:
+        SESSION.close()
